7305d75800bd60bd56de3706576bb0ab791b68ef
Jacob Stoebel <jstoebel@gmail.com>
2020-01-10 11:29:48 -0500

    notes on code smells

--- Theatrical-Players-Refactoring-Kata/end/src/statement.ts
+++ Theatrical-Players-Refactoring-Kata/end/src/statement.ts
@@ -1,17 +1,17 @@
 import {IInvoice, IPlays} from './types'
 
-export default function statement (invoice: IInvoice, plays: IPlays) {
-    let totalAmount = 0;
-    let volumeCredits = 0;
-    let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US",
+export default function statement (invoice: IInvoice, plays: IPlays) { // long function
+    let totalAmount = 0; // mutable
+    let volumeCredits = 0; // mutable
+    let result = `Statement for ${invoice.customer}\n`; // mutable, myterious name
+    const currencyFormatter = new Intl.NumberFormat("en-US",
         { style: "currency", currency: "USD",
-            minimumFractionDigits: 2 }).format;
+            minimumFractionDigits: 2 }).format; // myterious name
 
-    for (let perf of invoice.performances) {
+    for (let perf of invoice.performances) { // loop
         const play = plays[perf.playID];
-        let thisAmount = 0;
-        switch (play.type) {
+        let thisAmount = 0; // mutable, mysterious. the cost for a single performance
+        switch (play.type) { // switch!
             case "tragedy":
                 thisAmount = 40000;
                 if (perf.audience > 30) {
@@ -31,12 +31,12 @@ export default function statement (invoice: IInvoice, plays: IPlays) {
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
         // add extra credit for every ten comedy attendees
-        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5); // random business logic
         // print line for this order
-        result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+        result += ` ${play.name}: ${currencyFormatter(thisAmount/100)} (${perf.audience} seats)\n`;
         totalAmount += thisAmount;
     }
-    result += `Amount owed is ${format(totalAmount/100)}\n`;
+    result += `Amount owed is ${currencyFormatter(totalAmount/100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 }
